0f9d7e0c0e43066c2ea912e34bcf61c0109844f0
andreasrobert <andreasrobb23@gmail.com>
2021-08-31 03:25:38 +0700

    added guestDetails and commented out other menu

--- src/customApp/sidebar.js
+++ src/customApp/sidebar.js
@@ -1,13 +1,18 @@
 const options = [
   {
-    key: 'githubSearch',
-    label: 'sidebar.githubSearch',
-    leftIcon: 'ion-social-github'
-  },
-  {
-    key: 'blank_page',
-    label: 'sidebar.blankPage',
-    leftIcon: 'ion-document'
+    key: 'guestDetails',
+    label: 'sidebar.guestdetails',
+    leftIcon: 'ion-clipboard',
   }
+  // {
+  //   key: 'githubSearch',
+  //   label: 'sidebar.githubSearch',
+  //   leftIcon: 'ion-social-github'
+  // },
+  // {
+  //   key: 'blank_page',
+  //   label: 'sidebar.blankPage',
+  //   leftIcon: 'ion-document'
+  // }
 ];
 export default options;
